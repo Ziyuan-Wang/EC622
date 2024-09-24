6be20b0e7299eda505daa765c1ac3b2579fb7a74
Ziyuan-Wang <vvziyuan@gmail.com>
2024-09-23 20:05:35 -0700

    src and test updated

--- src/Ecosystem.jl
+++ src/Ecosystem.jl
@@ -1,5 +1,15 @@
 module Ecosystem
 
+"""
+The function computes a polynomial summation given coefficients and variable value.
+"""
 # Write your package code here.
+function polynomial(a,x)
+    out = 0 
+    for (ind, coef) in enumerate(a)
+        out += coef * x^(ind-1)
+    end
+    return out
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,6 @@ using Test
 
 @testset "Ecosystem.jl" begin
     # Write your tests here.
+    @test polynomial([−19,7,−4,6],3) == 128
+    @test polynomial([3,9,4,-2],5) == -102
 end
